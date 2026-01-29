6330514c3792969d852cff6a1fe550364db9d9f5
orpheus <orpheus@computerdream.club>
2026-01-29 01:23:50 +0000

    2026 wip

--- SETUP/issudo.sh
+++ SETUP/issudo.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 # TODO: can test if: "can sudo" instead
+# skipping for now since we prefer non root users anyway
+exit 0
 
-if [ `whoami` == "root" ]
-then
-    exit 0
-else
-    echo "You must be root to execute this command."
-    exit 1
-fi
+# if [ `whoami` == "root" ]
+# then
+#     exit 0
+# else
+#     echo "You must be root to execute this command."
+#     exit 1
+# fi
